d45e3a655aece6610a0b47c9766c0773ea95cf4d
atb <al.burns.email@gmail.com>
2017-09-15 10:30:51 -0700

    regex gen unfinished

--- tutorgame/regexapp/regex_maker.py
+++ tutorgame/regexapp/regex_maker.py
@@ -6,12 +6,17 @@ class Regexer:
     # This is a list of types and matching regex. This dictionary is used
     # to create the regex string and the findme string
     # {{
-    regex_dict = {'digit': '\d', 'single_char': '[a-zA-Z]', 'space': '\s'}
+    regex_dict = {
+        'digit': [True, '\d'],
+        'single_letter_all': [True, '[a-zA-Z]'],
+        'space': [True, '\s'],
+        'single_letter_one': [False, 
+        }
     # }}
 
     # This is a list of all opened types of regex
     # {{
-    open_regex = ['digit', 'single_char', 'space']
+    open_regex = ['digit', 'single_letter_all', 'space']
     # }}
 
     # This creates the Regexer object with args representing
@@ -27,15 +32,92 @@ class Regexer:
     # correspond for that type. Used for building answer strings.
     # {{
     def regex_step_function(self, func_key):
-        if func_key == 'single_char':
+        if func_key == 'single_letter_all':
             return random.choice(string.ascii_letters)
         elif func_key == 'digit':
             return str(random.randrange(0,10,1))
         elif func_key == 'space':
             return ' '
+        elif func_key == 'single_letter_one'
     # }}
 
     # This takes in a regex_list and shortens it, so
     # that two instances will return one instance
     # with a coefficient
     # {{ 
+    def compress_indicies(self, regex_list):
+        regex_string = ''
+        regex_list_compressed = []
+        how_many_indicies = 1
+        compress_to_to = False
+        print(str(regex_list))
+        for i in range(0, len(regex_list)):
+            if i == (len(regex_list) -1):
+                if compress_to_do:
+                    compressed_string = regex_list[i] + '{' + str(how_many_indicies) + '}'
+                    regex_list_compressed.append(compressed_string)
+                else:
+                    regex_list_compressed.append(regex_list[i])
+            elif regex_list[i] != regex_list[i+1]:
+                if compress_to_do:
+                    compressed_string = regex_list[i] + '{' + str(how_many_indicies) + '}'
+                    regex_list_compressed.append(compressed_string)
+                    how_many_indicies = 1
+                    compress_to_do = False
+                else:
+                    regex_list_compressed.append(regex_list[i])
+            else:
+                how_many_indicies += 1
+                compress_to_do = True
+        return regex_list_compressed
+    # }}
+
+    def regex_gen(self):
+        match_num = random.randrange(2, 5, 1)
+        selectme_string_list = []
+        for i in range(0, match_num):
+            selectme_string_list.append("")
+        regex_list = []
+        for i in range(0,9):
+            regex_step = random.choice(self.open_regex)
+            regex_list.append(self.regex_dict[regex_step])
+            for i in range(0, match_num):
+                selectme_string_list[i] += self.regex_step_function(regex_step)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
